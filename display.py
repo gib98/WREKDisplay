2753919eec958ed132c0fd67f49a933407fccc32
Gibran Essa <gibran.essa@gmail.com>
2017-10-20 15:12:44 -0400

    done?

--- display.py
+++ display.py
@@ -1,3 +1,4 @@
+import time
 from wrekscrape import *
 
 
@@ -8,11 +9,18 @@ class Fullscreen_Window:
         self.tk = Tk()
         self.tk.attributes('-zoomed', True)  # This just maximizes it so we can see the window. It's nothing to do with fullscreen.
         self.frame = Frame(self.tk)
-        self.frame.pack()
+        self.frame.grid()
         self.state = False
         self.tk.configure(background='white')
         self.tk.bind("<F11>", self.toggle_fullscreen)
-        self.tk.bind("<Escape>", self.end_fullscreen)
+        self.tk.bind("<Escape>", self.escape)
+
+    def escape(self, event=None):
+        exit()
+        self.tk.destroy
+        raise Exception("Exiting")
+
+
 
 
     def toggle_fullscreen(self, event=None):
@@ -27,16 +35,45 @@ class Fullscreen_Window:
         return "break"
 font1 = ("Helvetica", "50","bold")
 font2 = ("Helvetica", "50")
+font3 = ("Helvetica", "10")
+
+def pull():
+    s.update()
+    artistv.set(s.artist)
+    titlev.set(s.title)
+    albumv.set(s.album)
+    print("update",s.artist,s.title,s.album)
+    w.tk.after(5000,pull)
 
 if __name__ == '__main__':
+    s = Scraper()
     w = Fullscreen_Window()
     logo = PhotoImage(file="WREK_Logo.png")
     w.toggle_fullscreen()
-    img = Label(w.tk, image=logo, anchor = 'nw', background='white')
-    img.pack(fill='both' ,side = 'left')
-    a = Label(w.tk, text="Artist: ", font = font1, anchor = 'nw', background='white')
-    a.pack(side = 'left')
-    a1 = Label(w.tk, text = artist(), font = font2, background='white',  anchor = 'nw')
-    a1.pack(side = 'left')
+    artistv = StringVar()
+    titlev = StringVar()
+    albumv = StringVar()
+    s.update()
+    artistv.set(s.artist)
+    titlev.set(s.title)
+    albumv.set(s.album)
 
+    img = Label(w.tk, image=logo, background='white').grid(row=0,column=0, rowspan=2)
+
+    info = Label(w.tk, text="© Gibran Essa, 2017; may not work during specialty shows" , font = font3, background='white').grid(row=0,column=1,sticky='nw')
+
+    top = Label(w.tk, text=" Song Playing:" , font = font1, background='white').grid(row=1,column=1,columnspan=2,sticky='s')
+
+    a = Label(w.tk, text=" Artist: ", font = font1, background='white').grid(row=2,column=1,sticky='w')
+
+    a1 = Label(w.tk, textvariable = artistv, font = font2, background='white').grid(row=2,column=2,sticky='w')
+
+    t = Label(w.tk, text=" Title: ", font = font1, background='white').grid(row=3,column=1,sticky='w')
+
+    t1 = Label(w.tk, textvariable = titlev, font = font2, background='white').grid(row=3,column=2,sticky='w')
+
+    al = Label(w.tk, text=" Album: ", font = font1, background='white').grid(row=4,column=1,sticky='w')
+
+    al1 = Label(w.tk, textvariable = albumv, font = font2, background='white').grid(row=4,column=2,sticky='w')
+    pull()
     w.tk.mainloop()
--- wrekscrape.py
+++ wrekscrape.py
@@ -1,26 +1,24 @@
 from bs4 import BeautifulSoup as bs
 import requests as r
 
+class Scraper():
 
 
-"""
-return eturns the current playing song as 3 strings.
-format: Title, Artist, Album
-"""
-def scrape():
-    html = r.get("https://www.wrek.org/playlist/").text
-    soup = bs(html, 'html.parser')
-    output = soup.find_all('td')[1:4]
-    for i, j in enumerate(output):
-        output[i] = j.text
+    def __init__(self):
+        self.title, self.artist, self.album  = self.scrape()
 
-    return output[0], output[1], output[2]
+    def update(self):
+        self.title, self.artist, self.album  = self.scrape()
 
-def title():
-    return scrape()[0]
+    """
+    return eturns the current playing song as 3 strings.
+    format: Title, Artist, Album
+    """
+    def scrape(self):
+        html = r.get("https://www.wrek.org/playlist/").text
+        soup = bs(html, 'html.parser')
+        output = soup.find_all('td')[1:4]
+        for i, j in enumerate(output):
+            output[i] = j.text
 
-def artist():
-    return scrape()[1]
-
-def album():
-    return scrape()[2]
+        return output[0], output[1], output[2]
