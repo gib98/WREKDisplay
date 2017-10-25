6f00fef0c705415b0b2a8257e2718c9645bad434
Gibran Essa <gibran.essa@gmail.com>
2017-10-24 20:56:58 -0400

    jack is a nerd

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='WREKDisplay',
+      version='1.0',
+      description='Displays song playing on WREK',
+      author='Gibran Essa',
+      author_email='gibran.essa@gmail.com',
+      url='https://github.com/gib98/WREKDisplay'
+)
